3478c303400f9443f52fa802a0a51bc91db8d859
Jan Schormann <jan@eucli.de>
2015-07-08 20:38:01 +0200

    config

--- plugins/trello_burndown.py
+++ plugins/trello_burndown.py
@@ -1,10 +1,33 @@
 #!/usr/bin/env python
 
-def config():
-    return "graph_title Trello Burndown Chart"
+countListNames = [ 'todo', 'doing', 'done', ]
+colours = [ 2, 1, 0, ]
+
+def config(out):
+    print >> out, "graph_title Trello Burndown Chart"
+    print >> out, "graph_vlabel cards"
+    print >> out, "graph_category brainlab"
+    print >> out, "graph_args --lower-limit 0"
+
+    areaOrStack='AREA'
+    for name, colour in zip(countListNames, colours):
+        print >> out, "%s.label %s" % (name, name.capitalize())
+        print >> out, "%s.draw %s" % (name, areaOrStack)
+        print >> out, "%s.colour COLOUR%d" % (name, colour)
+        areaOrStack = 'STACK'
+        continue
+    return
+
+def main():
+    if len(sys.argv) == 2 and sys.argv[1] == 'config':
+        config(sys.stdout)
+        sys.exit(0)
+        pass
+    return
+
 
 # Todo:
 # describe necessary structure in trello
-# pick api key from env
-# config
+# pick api key and board id from env
 # values
+# add cooldown
--- plugins/trello_burndown_test.py
+++ plugins/trello_burndown_test.py
@@ -1,11 +1,22 @@
 import unittest
+import cStringIO
 
 import trello_burndown
 
 class TrelloBurndownTest(unittest.TestCase):
-    def test_config(self):
-        config = trello_burndown.config()
-        self.assertIn("graph_title", config)
+    def test_config_has_title(self):
+        out = cStringIO.StringIO()
+        trello_burndown.config(out)
+        self.assertIn("graph_title", out.getvalue())
+        pass
+
+    def test_config_has_labels(self):
+        out = cStringIO.StringIO()
+        trello_burndown.config(out)
+        for area in "todo", "doing", "done":
+            self.assertIn(area + ".label", out.getvalue())
+            continue
+
         pass
 
     pass
