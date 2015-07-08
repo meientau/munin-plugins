38aae668c4a5a3e027ea70db37c7494b1ba8fdd6
Jan Schormann <jan@eucli.de>
2015-07-08 20:18:25 +0200

    first test fails

--- plugins/trello_burndown_test.py
+++ plugins/trello_burndown_test.py
@@ -4,8 +4,11 @@ import trello_burndown
 
 class TrelloBurndownTest(unittest.TestCase):
     def test_config(self):
+        config = trello_burndown.config()
+        self.assertIn("graph_title", config)
         pass
 
     pass
 
-    
+if __name__ == '__main__':
+    unittest.main()
