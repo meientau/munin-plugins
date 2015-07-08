4523b6bedc937d23eea7e986ef43d86258af5d85
Jan Schormann <jan@eucli.de>
2015-07-08 20:39:17 +0200

    pulled out .out

--- plugins/trello_burndown_test.py
+++ plugins/trello_burndown_test.py
@@ -4,19 +4,20 @@ import cStringIO
 import trello_burndown
 
 class TrelloBurndownTest(unittest.TestCase):
+    def setUp(self):
+        self.out = cStringIO.StringIO()
+        return
+
     def test_config_has_title(self):
-        out = cStringIO.StringIO()
         trello_burndown.config(out)
-        self.assertIn("graph_title", out.getvalue())
+        self.assertIn("graph_title", self.out.getvalue())
         pass
 
     def test_config_has_labels(self):
-        out = cStringIO.StringIO()
         trello_burndown.config(out)
         for area in "todo", "doing", "done":
-            self.assertIn(area + ".label", out.getvalue())
+            self.assertIn(area + ".label", self.out.getvalue())
             continue
-
         pass
 
     pass
